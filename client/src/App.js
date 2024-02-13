4d84f83ebb566df33d054184fcdbd693a0d19f00
Vincent Tran <vintran2015@gmail.com>
2024-02-13 13:18:03 -0500

    Updated client

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import { useState } from 'react';
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function TempHeader() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  )
+}
 
 function App() {
 
+  // useState is a VERY important concept here, please read up on it!
+  // https://react.dev/reference/react/useState
   const [userData, setData] = useState({
     username: null,
     password: null
   });
+  // we will also be using this to PASS STATE DOWN (another really important concept)
+  // https://react.dev/learn/sharing-state-between-components
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <TempHeader />
+    },
+  ]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
