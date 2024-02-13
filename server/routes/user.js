ac3c74f4a64a29e26a79d262117dde88b6c1c76f
Vincent Tran <vintran2015@gmail.com>
2024-02-13 11:26:34 -0500

    Updated user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
-const { Router } = require("express");
+const express = require("express");
 const mongo = require("../db/connection");
+const router = express.Router();
 
 var db = mongo.getDb();
-
