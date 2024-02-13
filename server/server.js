73a4ab692870b4e13dc2d76c1787dff9bf8e3916
Vincent Tran <vintran2015@gmail.com>
2024-02-13 02:03:41 -0500

    Updated server/db/connection.js, server/db/server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
-const cors = require('cors');
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;  // use whatever is specified in PORT, or 5000 if PORT is null
+app.use(cors());
+app.use(express.json());
+
+const mongo = require("./db/connection");
+
+// connect to database with driver connection
+app.listen(port, () => {
+    mongo.establishConnection( function (callback) {
+        if (callback) {
+            console.error("Unable to connect to MongoDB.");
+        }
+        else {
+            console.log("Established connection with MongoDB.");
+        }
+    });
+    console.log(`Server running on port: ${port}`);
+});
