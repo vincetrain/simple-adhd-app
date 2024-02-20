3ca67192e747f59e1d85fe2e0cbaa8a72b7b02d5
Vincent Tran <vintran2015@gmail.com>
2024-02-20 12:04:35 -0500

    restructed project, added pages and components, todo move stuff from App.js

--- client/src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar(user) {
-  return (
-    <nav className="max-w-md ml-0 mr-auto bg-white">
-      <h1>This is a test</h1>
-    </nav>
-  );
-}
---
+++ client/src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+import "./Welcome.scss";
+
+import Login from "../../components/Login/Login";
+
+export default function Welcome(user, setUserData) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
